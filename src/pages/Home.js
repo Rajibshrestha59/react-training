55278d310921e94330c9377943b775c59e7b205f
Rajib Shresth <rajibstha059@gmail.com>
2024-04-20 14:50:17 +0545

    last day user list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ButtonComponent from "../components/Button";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [users, setUsers] = useState([]);
+  const getAllUsers = async () => {
+    try {
+      const response = await axios({
+        url: "http://localhost:8080/users",
+        method: "GET",
+      });
+      console.log("response in get user", response);
+      setUsers(response.data);
+      return response.data;
+    } catch (err) {
+      console.log("error in get user", err);
+    }
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+  return (
+    <div>
+      {/* <ButtonComponent onClick={getAllUsers} buttonText="Get all users" /> */}
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((item, index) => (
+            <tr>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.emailAddress}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Home;
